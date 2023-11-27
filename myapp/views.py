60ce680ffeb530a44234c03ffbb4f52741944de9
Qobil <sh.yuldashbekov@gmail.com>
2023-11-27 05:05:02 +0500

    first commit

--- myapp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import *
-
-def main(request):
-    posts = Post.objects.all()
-    return render(request, template_name = 'main.html', context = {})
